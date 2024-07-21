9a2f8267e83af2172018eb46acf7f079c441a33a
madhav-v <madhavdhungana36@gmail.com>
2024-07-21 21:45:16 +0545

    Posts

--- backend/src/controllers/post.controller.ts
+++ backend/src/controllers/post.controller.ts
@@ -1,8 +1,20 @@
+import { Request, Response } from "express";
+import ErrorHandler from "../utils/errorhandler";
+import { getRepository } from "typeorm";
+import logger from "../config/logger";
+import { Org } from "../models/Org.model";
+import cloudinary from "../config/cloudinary.config";
+import { CustomRequest } from "../middleware/auth.middleware";
+import { User } from "../models/User.model";
+
 export class PostController {
   createPost = async (req: Request, res: Response) => {
     try {
-    } catch (error: any) {
+    } catch (error: any) {}
+  };
 
-    }
+  getAllPosts = async (req: Request, res: Response) => {
+    try {
+    } catch (error: any) {}
   };
 }
